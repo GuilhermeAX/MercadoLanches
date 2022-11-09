d85d5d36e7d450e58845842530bab1236b01f396
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-11-09 12:59:36 -0300

    Implementando novos metodos

--- src/controller/InterfaceProduto.java
+++ src/controller/InterfaceProduto.java
@@ -1,8 +1,13 @@
 package controller;
 
+import model.Produto;
+
 public interface InterfaceProduto {
 	public void salvarProduto();
 	public void atualizarProduto();
 	public void listarProdutos();
+	public Produto buscarNumeroProduto(int id);
+	public void excluirProduto(int id);
+	public int gerarNumero();
 
 }
